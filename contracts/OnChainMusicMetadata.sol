9be5ef61cb10102aa59ec629aed9b38666037d4b
Sweets Sweetman <sweetmantech@gmail.com>
2022-07-29 09:30:38 -0300

    I add MVP: get Base64 JSON to show up in OpenSea.

--- contracts/OnChainMusicMetadata.sol
+++ contracts/OnChainMusicMetadata.sol
@@ -1,38 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
-
-contract OnChainMusicMetadata {
-    uint256 public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint256 amount, uint256 when);
-
-    constructor(uint256 _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
-}
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./OnChainMetadata.sol";
 
@@ -40,6 +8,7 @@ import "./OnChainMetadata.sol";
  * @title On-chain metadata for ERC721,
  * making quick and easy to create html/js NFTs, parametric NFTs or any NFT with dynamic metadata.
  * @author Daniel Gonzalez Abalde aka @DGANFT aka DaniGA#9856.
+ * OG Author's code: https://github.com/DanielAbalde/NFT-On-Chain-Metadata
  */
 contract OnChainMusicMetadata is ERC721, OnChainMetadata {
     constructor(string memory name, string memory symbol)
