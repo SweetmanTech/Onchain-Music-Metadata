34e4676d3a37ac44813418e5ee5dd8ddf8092cc0
Sweets Sweetman <sweetmantech@gmail.com>
2022-07-30 19:27:29 -0300

    I update example ISBC

--- contracts/MusicStructs.sol
+++ contracts/MusicStructs.sol
@@ -30,7 +30,7 @@ contract MusicStructs {
     struct AudioQualitative {
         string license; // CC0
         string externalUrl; // Link to your project website
-        string isrc; // 1234567890123
+        string isrc; // CC-XXX-YY-NNNNN
         string genre; // Rock / Pop / Metal / Hip-Hop / Electronic / Classical / Jazz / Folk / Reggae / Other
     }
 
